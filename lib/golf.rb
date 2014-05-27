64e72c238368d018be6d3c3b95446f291b356d84
amcaplan <ariel.caplan@mail.yu.edu>
2014-05-27 19:30:32 -0400

    Working solutions for 1-5

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,32 @@
 class Golf
+  class << self
+    def hole1(a)
+      a.inject(:*)
+    end
+
+    def hole2
+      (?a..?z).to_a
+    end
+
+    def hole3(a)
+      (a.downto 1).inject(:*)
+    end
+
+    def hole4(a)
+      a.map{|x|
+        case x[0..2]
+        when "man"
+          "hat(#{x})"
+        when "dog"
+          x.chop<<'(bone))'
+        else
+          "dead#{x[3..-1]}"
+        end
+      }
+    end
+
+    def hole5(a)
+      (1..a.size).flat_map{|n|a.each_cons(n).to_a}
+    end
+  end
 end
