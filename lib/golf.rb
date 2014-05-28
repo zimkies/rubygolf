c80b6913cac975787121c4b81d5688600d55bd6f
amcaplan <ariel.caplan@mail.yu.edu>
2014-05-27 20:28:37 -0400

    Remove a few more characters

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,18 +1,18 @@
 class Golf
   class << self
-    def hole1(a)
-      a.inject(:*)
+    def hole1 a
+      a.inject :*
     end
 
     def hole2
       (?a..?z).to_a
     end
 
-    def hole3(a)
-      (a.downto 1).inject(:*)
+    def hole3 a
+      (1..a).inject(:*)
     end
 
-    def hole4(a)
+    def hole4 a
       a.map{|x|
         case x[0..2]
         when "man"
@@ -25,11 +25,11 @@ class Golf
       }
     end
 
-    def hole5(a)
+    def hole5 a
       (1..a.size).flat_map{|n|a.each_cons(n).to_a}
     end
 
-    def hole6(n)
+    def hole6 n
       (1..n).map { |a|
         s = ''
         s << 'fizz' if a % 3 == 0
@@ -40,19 +40,19 @@ class Golf
 
     def hole7 a
       (r = []) << a[0].to_s
-      a.each_with_index { |i, x| a[x+1] != i+1 ? (r.last<<"-#{i}" if !(i == r[-1].to_i); r << "#{a[x+1]}") : p }
+      a.map.with_index { |i, x| a[x+1] != i+1 ? (r[-1]<<"-#{i}" if !(i == r[-1].to_i); r << "#{a[x+1]}") : p }
       r.pop
       r
     end
 
-    def hole8(n)
+    def hole8 n
       return n<2 ? [1]:[1,1] if n < 3
       a=hole8 n-1
       a<<a[-1]+a[-2]
     end
 
-    def hole9(s)
-      s.split.map { |w| w.length < 11 ? w : "#{w[0..3]}...#{w[-3..-1]}" }.join ' '
+    def hole9 s
+      s.split.map { |w| w.size < 11 ? w : "#{w[0..3]}...#{w[-3..-1]}" }.join ' '
     end
   end
 end
