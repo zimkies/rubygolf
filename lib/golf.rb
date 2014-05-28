4f9a4564b0d3677e27f3d60e782b54fd669105d7
hoffmanilya <ilya@synapseautomation.com>
2014-05-27 17:05:09 -0700

    hole7

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,11 @@
 class Golf
+
+  class << self
+    def hole7 a
+      (r = []) << a[0].to_s
+      a.each_with_index { |i,x| a[x+1] != i+1 ? (r.last<<"-#{i}" unless i == r[-1].to_i; r << "#{a[x+1]}") : p }
+      r.pop
+      r
+    end
+  end
 end
